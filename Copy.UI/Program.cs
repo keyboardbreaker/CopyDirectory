2f1f9c458f03e743dcd8e922be9a9dd6c33a45d2
Yu-Jin <yujinost@gmail.com>
2018-04-18 16:39:02 +0100

    catching errors in UI

--- Copy.UI/Program.cs
+++ Copy.UI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CopyDirectory;
+using System.IO;
 
 namespace Copy.UI
 {
@@ -8,23 +9,50 @@ namespace Copy.UI
         static void Main(string[] args)
         {
             HandleCopy hc = new HandleCopy();
-            var path = @"C:\temp\newfolder";
-            var newPath = @"C:\temp\newfolder1";
+            //var path = @"C:\temp\newfolder";
+            //var newPath = @"C:\temp\newfolder1";
 
-            //6. Users must be able to pick or specify a source and target directory.
-            Console.WriteLine("Please specify source to copy: ");
-            var userSource = Console.ReadLine().Trim();
-            Console.WriteLine("Please specify target to copy: ");
-            var userTarget = Console.ReadLine().Trim();
+            do
+            {
 
-            try
-            {
-                hc.CopyFolder(path, newPath, true);
+                //6. Users must be able to pick or specify a source and target directory.
+                Console.WriteLine("Please specify source to copy: ");
+                var userSource = Console.ReadLine().Trim();
+                if (Directory.Exists(userSource))
+                {
+                    Console.WriteLine("Please specify target to copy to: ");
+                    var userTarget = Console.ReadLine().Trim();
+
+                    try
+                    {
+                        hc.CopyFolder(userSource, userTarget, true);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Invalid paths");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("The source directory does not exist. ");
+                }
+
+                Console.WriteLine("Do you wish to continue y/n");
+                var again = Console.ReadLine().Trim();
+                if (again == "n")
+                    break;
+                else if(again == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("I dont know that command, continue");
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            while (true);
         }
     }
 }
